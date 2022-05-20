3d88210eab67553a31afeba1bf066980c07b3d3f
felipe.magagnin <felipebmagagnin@gmail.com>
2022-05-20 17:42:42 -0300

    improve website look

--- components/header.js
+++ components/header.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+import { Fragment } from 'react'
 
 export default function Header({ wallet, isReady, address, staticPage }) {
     return (
-        <div className="header">
+        <Fragment>
             <div className="scrolling-image-container">
                 <div className="scrolling-image"></div>
             </div>
 
+            <div className="header">
             <div className='menus'>
                 <div className="primary-navigation">
                     <ul style={{ padding: 0 }}>
                         <li>
                             <a href="/view">View</a>
-                        </li>
-
-                        <li>
-                            <a href="/whitelist">Whitelist</a>
                         </li>
 
                         <li>
@@ -32,25 +30,23 @@ export default function Header({ wallet, isReady, address, staticPage }) {
         
             <div>
                 <a href='https://discord.gg/cDh6gbn59A' className='mr-10' target='_blank' rel="noreferrer">
-                    <Image src='/assets/discord.svg' alt='discord' width='20' height='20' />
+                    <Image src='/assets/discord.svg' alt='discord' width='30' height='30' />
                 </a>
 
                 <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
-                    <Image src='/assets/twitter.svg' alt='twitter' width='20' height='20' />
+                    <Image src='/assets/twitter.svg' alt='twitter' width='30' height='30' />
                 </a>
 
-                <a href='https://rinkeby.etherscan.io/address/0xaaf7cc2a92c8d60ccf73e6cef503fdcd69917431' className='mr-10' target='_blank' rel="noreferrer">
-                    <Image src='/assets/fantom.svg' alt='ftmscan' width='20' height='20' />
+                <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
+                    <Image src='/assets/instagram.png' alt='twitter' width='30' height='30' />
                 </a>
 
-                <a href='https://rinkeby.etherscan.io/address/0xaaf7cc2a92c8d60ccf73e6cef503fdcd69917431' className='mr-10' target='_blank' rel="noreferrer">
-                    <Image src='/assets/fantom.svg' alt='ftmscan' width='20' height='20' />
-                </a>
             </div>
 
             {!staticPage && <button className='button' onClick={wallet}>
                 {isReady ? address?.substring(0, 6) + "..." + address?.substring(address.length - 4, address.length) : "Connect"} { }
             </button>}
         </div>
+        </Fragment>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -15,29 +15,39 @@ function Index() {
 
       <div id="app">
         <img src='/assets/bad-balloons-logo.png' alt='discord' style={{ width: '20vh' }} />
-        <div className="form-container">
-          <div className='content-container'>
 
-            <div style={{ margin: 20 }}>
-              <div style={{ flex: 1 }} className='image-frame' style={{ height: 300 }}>
-                <Image src='/assets/balloons.jpeg' alt='discord' width='300' height='300' />
+        <div style={{ display: 'flex', width: '80%'}} >
+          <div style={{width: '50%', marginRight: '10px'}}>
+            <img src='/assets/balloon1.jpeg' style={{ display: 'inline', width: '50%', height: '100%', borderRadius: '20px 0px 0px 20px' }} className='image-frame' />
+            <img src='/assets/balloon2.jpeg' style={{ display: 'inline', width: '50%', height: '100%', borderRadius: '0px 20px 20px 0px' }} className='image-frame'/>
+          </div>
+
+          <div className="form-container" style={{width: '50%', height: '100%'}}>
+            <div className='content-container'>
+
+              <div style={{ margin: 20 }}>
+                
+              </div>
+
+              <div style={{ flex: 1, marginTop: 10 }}>
+
+                Bad Balloons is a collection of <div className='colorGradient'>3333</div> random generated Ballons stored on the <div className='colorGradient'>Ethereum</div> Blockchain.
+
               </div>
             </div>
 
-            <div style={{ flex: 1, marginTop: 10 }}>
+            <br />
 
-              Bad Balloons is a collection of <div className='colorGradient'>3333</div> random generated Ballons stored on the <div className='colorGradient'>Ethereum</div> Blockchain.
+            <button className='button' style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 20, width: 300 }}>
+              Whitelist
+            </button>
 
-            </div>
           </div>
-
-          <br />
-
-          <button className='button' style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 20, width: 300 }}>
-            Whitelist
-          </button>
-
         </div>
+        
+        
+        
+        
       </div>
     </Fragment>
   )
