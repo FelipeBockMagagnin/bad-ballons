62b0f23973235e801b6a327133dec41814cae4d9
felipe.magagnin <felipebmagagnin@gmail.com>
2022-05-25 11:23:32 -0300

    Improve site quality

--- components/header.js
+++ components/header.js
@@ -5,10 +5,6 @@ import { Fragment } from 'react'
 export default function Header({ wallet, isReady, address, staticPage }) {
     return (
         <Fragment>
-            <div className="scrolling-image-container">
-                <div className="scrolling-image"></div>
-            </div>
-
             <div className="header">
             <div className='menus'>
                 <div className="primary-navigation">
@@ -18,28 +14,18 @@ export default function Header({ wallet, isReady, address, staticPage }) {
                         </li>
 
                         <li>
-                            <a href="/roadmap">Roadmap</a>
+                            <a href="/history">history</a>
                         </li>
 
                         <li>
-                            <a href="/about">About</a>
+                            <a href="/roadmap">Roadmap</a>
                         </li>
                     </ul>
                 </div>
             </div>
         
             <div>
-                <a href='https://discord.gg/cDh6gbn59A' className='mr-10' target='_blank' rel="noreferrer">
-                    <Image src='/assets/discord.svg' alt='discord' width='30' height='30' />
-                </a>
-
-                <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
-                    <Image src='/assets/twitter.svg' alt='twitter' width='30' height='30' />
-                </a>
-
-                <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
-                    <Image src='/assets/instagram.png' alt='twitter' width='30' height='30' />
-                </a>
+                
 
             </div>
 
--- pages/index.js
+++ pages/index.js
@@ -7,47 +7,157 @@ import Image from 'next/image'
 
 function Index() {
 
+  function connectWallet() {
+    Swal.fire({
+      title: 'Mint Date TBD!',
+      html: 'Subscribe to our whitelist',
+      icon: 'none',
+      confirmButtonText: 'Ok'
+    })
+  }
+
   return (
     <Fragment>
       <div className="geeks"></div>
 
-      <Header wallet={null} isReady={null} address={null} staticPage={true} />
+      <Header wallet={connectWallet} isReady={null} address={null} staticPage={false} />
 
-      <div id="app">
-        <img src='/assets/bad-balloons-logo.png' alt='discord' style={{ width: '20vh' }} />
+      <div className="container">
+        <div className="logo" style={{textAlign: 'center'}}>
+          <img src='/assets/bad-balloons-logo.png' alt='logo' style={{ width: '30vh' }} />
+        </div>
 
-        <div style={{ display: 'flex', width: '80%'}} >
-          <div style={{width: '50%', marginRight: '10px'}}>
-            <img src='/assets/balloon1.jpeg' style={{ display: 'inline', width: '50%', height: '100%', borderRadius: '20px 0px 0px 20px' }} className='image-frame' />
-            <img src='/assets/balloon2.jpeg' style={{ display: 'inline', width: '50%', height: '100%', borderRadius: '0px 20px 20px 0px' }} className='image-frame'/>
-          </div>
+        <div className="planet center-grid">
+          <img src='/assets/planet.png' alt='logo' style={{ width: '75%' }} />
+        </div>
 
-          <div className="form-container" style={{width: '50%', height: '100%'}}>
+        <div className="balloon center-grid">
+          <img src='/assets/balloon1.jpeg' style={{ display: 'inline', width: '75%', height: 'auto', borderRadius: '20px' }} className='image-frame' />
+        </div>
+
+        <div className="whitelist top-center-grid">
+          <div className="form-container" style={{ margin: '10px', width: '50%' }}>
             <div className='content-container'>
 
               <div style={{ margin: 20 }}>
-                
+
               </div>
 
-              <div style={{ flex: 1, marginTop: 10 }}>
+              <div style={{ flex: 1, marginTop: 10, textAlign: 'justify' }}>
 
                 Bad Balloons is a collection of <div className='colorGradient'>3333</div> random generated Ballons stored on the <div className='colorGradient'>Ethereum</div> Blockchain.
 
+                
               </div>
+
+              <br />
+
+              <button className='button' style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 20 }}>
+                Whitelist
+              </button>
+
+              <div style={{marginTop: '20px'}}>
+                  <a href='https://discord.gg/cDh6gbn59A' className='mr-10' target='_blank' rel="noreferrer">
+                    <Image src='/assets/discord.svg' alt='discord' width='30' height='30' />
+                  </a>
+
+                  <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
+                    <Image src='/assets/twitter.svg' alt='twitter' width='30' height='30' />
+                  </a>
+
+                  <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
+                    <Image src='/assets/instagram.png' alt='twitter' width='30' height='30' />
+                  </a>
+                </div>
             </div>
-
-            <br />
-
-            <button className='button' style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 20, width: 300 }}>
-              Whitelist
-            </button>
-
           </div>
         </div>
-        
-        
-        
-        
+
+        <div class="roadmap top-center-grid" style={{color: 'white', textAlign: 'center'}}>
+          <div>
+            Roadmap
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            item
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+          </div>
+        </div>
+
+
+        <div class="galaxy bottom-right-grid">
+          <img src='/assets/portal.png' className='image-frame' />
+        </div>
       </div>
     </Fragment>
   )
