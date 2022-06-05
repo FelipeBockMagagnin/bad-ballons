f04fbadde71e08404b627a1849a325af293199b6
felipe.magagnin <felipebmagagnin@gmail.com>
2022-06-05 14:52:26 -0300

    add fontawesome and improve website

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Header from '../components/header'
 import abiObj from '../src/utils/abi'
 const Web3 = require('web3')
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faDiscord, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 function Index() {
 
@@ -23,8 +26,8 @@ function Index() {
       <Header wallet={connectWallet} isReady={null} address={null} staticPage={false} />
 
       <div className="container">
-        <div className="logo" style={{textAlign: 'center'}}>
-          <img src='/assets/logo.gif' alt='logo' style={{ width: '30vh' }} />
+        <div className="logo" style={{ textAlign: 'center' }}>
+          <img src='/assets/logo.gif' alt='logo' style={{ width: '60vh' }} />
         </div>
 
         <div className="planet">
@@ -44,7 +47,42 @@ function Index() {
               </div>
 
               <div style={{ flex: 1, marginTop: 10, textAlign: 'justify' }}>
-                Bad Balloons is a collection of <div className='colorGradient'>3333</div> random generated Ballons stored on the <div className='colorGradient'>Ethereum</div> Blockchain.
+                A collection of <div className='colorGradient'>XXXX</div> random generated Ballons stored on the <div className='colorGradient'>Ethereum</div> Blockchain for <div className='colorGradient'>0.XX ETH</div> each.
+              </div>
+
+              <div>
+                <br />
+                <div className='colorGradient'>MINTING REWARDS:</div>
+
+                <br />
+                - 1/1 Token Reward up to $25K in ETH
+                (By mint one of the 5 special golden balloons, get $5k ETH in your wallet)
+
+                <br />
+                - Exclusive discord giveaways every week!
+
+                <br />
+                - 3% of Transaction Royalties go to whoever originally minted
+
+                <br />
+                <br />
+
+                <div className='colorGradient'>HOLDING REWARDS</div>
+
+                <br />
+                - AirDrops
+
+                <br />
+                - Holders Travels Giveaway (3 trips to anywhere in the world all paid for)
+
+                <br />
+                - (DAO Voting Power) We will create a system for members that acquires an NFT have a voice in the next project decisions
+
+                <br />
+                - Exclusive access to the Bad Store with discounted Balloons custom clothing items.
+
+                <br />
+                - 3D NFTs Ready for Metaverse
               </div>
 
               <br />
@@ -53,107 +91,107 @@ function Index() {
                 Whitelist
               </button>
 
-              <div style={{marginTop: '20px'}}>
-                  <a href='https://discord.gg/cDh6gbn59A' className='mr-10' target='_blank' rel="noreferrer">
-                    <Image src='/assets/discord.svg' alt='discord' width='30' height='30' />
-                  </a>
+              <div style={{ marginTop: '20px' }}>
+                <a href='https://discord.com/invite/YV7ezZsUkV' className='mr-10' target='_blank' rel="noreferrer">
+                  <FontAwesomeIcon icon={faDiscord} size='2x' />
+                </a>
 
-                  <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
-                    <Image src='/assets/twitter.svg' alt='twitter' width='30' height='30' />
-                  </a>
+                <a href='https://twitter.com/badballoonsNFT' target='_blank' className='mr-10' rel="noreferrer">
+                  <FontAwesomeIcon icon={faTwitter} size='2x' />
+                </a>
 
-                  <a href='https://twitter.com/FantomChess' target='_blank' className='mr-10' rel="noreferrer">
-                    <Image src='/assets/instagram.png' alt='twitter' width='30' height='30' />
-                  </a>
-                </div>
+                <a href='https://www.instagram.com/badballoonsnft/' target='_blank' className='mr-10' rel="noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} size='2x' />
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="roadmap top-center-grid" style={{color: 'white', textAlign: 'center'}}>
+        <div class="roadmap top-center-grid" style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>
           <div>
-            Roadmap
+            <h1 className='colorGradient'>Roadmap</h1>
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 1. Launch of the collection
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 2. Connection with the community
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 3. Altruistic Movement
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 4. Balloons Brand
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 5. Metaverse
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-            item
+            <FontAwesomeIcon icon={faSpinner} size='2x' className="spinner" /> 6. Game NFT Bad Balloons
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
         </div>
 
 
         <div class="galaxy bottom-right-grid">
-          <img src='/assets/portal.png' className='image-frame' style={{width: 'auto'}} />
+          <img src='/assets/portal.png' className='image-frame' style={{ width: 'auto' }} />
         </div>
       </div>
       <div className='stars'></div>
